cf4f58e2237a434a731b0b1e9f905d064bc436e8
LukaW7konta <wosek07@gmail.com>
2024-01-10 19:02:06 +0100

    Dzień 4

--- Apka2/Apka2/Program.cs
+++ Apka2/Apka2/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+// liczby całkowite
+int myAge2 = 120;
+int newAge = myAge2 + 5;
+
+int myVar = int.MinValue;
+uint myVar2 = uint.MaxValue;
+ulong myVar3 = ulong.MaxValue;
+// liczby zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+double myNumber2 = double.MaxValue;
+decimal myNumber3 = decimal.MaxValue;
+
+// zmienne tekstowe
+string name = "Luka";
+string surname = "1";
+string result = name + surname + myAge2;
+// Console.WriteLine(result);
+char myVar4 = 'c';
+var result2 = name.ToArray();
+
+// zmienna logiczna
+bool isActive = true;
+isActive = false;
+var isValid = 5 > 6;
+Console.WriteLine(isValid);
