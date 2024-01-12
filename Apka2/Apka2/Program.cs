08c116e0114ae9bae6dc9c65564ee8b546d27012
LukaW7konta <wosek07@gmail.com>
2024-01-12 14:49:36 +0100

    Dzień4

--- Apka2/Apka2/Program.cs
+++ Apka2/Apka2/Program.cs
@@ -1,26 +1,16 @@
-﻿
-// liczby całkowite
-int myAge2 = 120;
-int newAge = myAge2 + 5;
+﻿var name = "OLA";
+var gender = "WOMAN";
+var age = 29;
 
-int myVar = int.MinValue;
-uint myVar2 = uint.MaxValue;
-ulong myVar3 = ulong.MaxValue;
-// liczby zmiennoprzecinkowe
-float myNumber = float.MaxValue;
-double myNumber2 = double.MaxValue;
-decimal myNumber3 = decimal.MaxValue;
-
-// zmienne tekstowe
-string name = "Luka";
-string surname = "1";
-string result = name + surname + myAge2;
-// Console.WriteLine(result);
-char myVar4 = 'c';
-var result2 = name.ToArray();
-
-// zmienna logiczna
-bool isActive = true;
-isActive = false;
-var isValid = 5 > 6;
-Console.WriteLine(isValid);
+if (gender == "WOMAN" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 roku życia");
+}
+else if (name == "OLA" && age == 29)
+{
+    Console.WriteLine("OLA, LAT 29");
+}
+else if (gender == "MAN" && age <18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
